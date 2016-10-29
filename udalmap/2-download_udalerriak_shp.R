3faaa2d8fc31ec32328ec2bee52f0376714ccd42
gkobeaga <gkobeaga@riseup.net>
2016-10-29 17:39:06 +0200

    Update downloading and reading data

--- udalmap/2-download_udalerriak_shp.R
+++ udalmap/2-download_udalerriak_shp.R
@@ -1,11 +1,13 @@
 library(maptools)
 
 ## Download shape file and unzip
-udalerriak.zip <- "./udalerriak.zip"
-download.file("http://www.euskalgeo.net/sites/euskalgeo.net/files/fitxategi-eranskin/udalerriak_0.zip", udalerriak.zip)
-unzip(udalerriak.zip, exdir="udalerriak")
+if (!file.exists("udalerriak/udalerriak.shp"))
+{ udalerriak.url <- "http://www.euskalgeo.net/sites/euskalgeo.net/files/fitxategi-eranskin/udalerriak_0.zip"
+  download.file(udalerriak.url, "udalerriak.zip")
+  unzip("udalerriak.zip", exdir="udalerriak")
+}
 
-# Load the shape file to R
+## Load the shape file to R
 udalerriak.shape<-readShapePoly("udalerriak/udalerriak.shp")
 
 ## Change encoding from Windows-1252 to UTF-8
